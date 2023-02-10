654cdb9faf0a4be4683fe482b44c64ccc873c15e
fmntnz <C0ndeAstar0th@gmail.com>
2023-02-10 16:27:41 -0500

    Class02

--- 02_crud_set.py
+++ 02_crud_set.py
@@ -12,20 +12,22 @@ print(set_countries)
 set_countries.add('pe')
 print(set_countries)
 
-# update
-set_countries.update({'ar', 'ecua', 'pe'})
+# update - We couild add more items than in an Add
+set_countries.update({'ar', 'ec', 'pe'})
 print(set_countries)
 
 # remove
 
 set_countries.remove('col')
 print(set_countries)
-
+#remove shows an error
 set_countries.remove('ar')
+#discard does not break the program
 set_countries.discard('arg')
 print(set_countries)
 set_countries.add('arg')
 print(set_countries)
+#delete all items from a set
 set_countries.clear()
 print(set_countries)
 print(len(set_countries))
