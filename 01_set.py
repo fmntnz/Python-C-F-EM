6ff31f70acc6a7da67e4d8e156b34718ebd7195b
fmntnz <C0ndeAstar0th@gmail.com>
2023-02-10 16:21:22 -0500

    Class01

--- 01_set.py
+++ 01_set.py
@@ -1,17 +1,18 @@
+'''Conjuntos o Sets Diferente de Diccionarios'''
 set_countries = {'col', 'mex', 'bol'}
 print(set_countries)
 print(type(set_countries))
 
 set_numbers = {1, 2, 2, 443, 23}
 print(set_numbers)
-
-set_types = {1, 'hola', False, 12.12}
+'''El Conjunto Nunca es Ordenado'''
+set_types = {5, 'Hello', False, 13.23}
 print(set_types)
 
-set_from_string = set('hoola')
+set_from_string = set('Freddy Norberto')
 print(set_from_string)
 
-set_from_tuples = set(('abc', 'cbv', 'as', 'abc'))
+set_from_tuples = set(('abc', 'ase', 'assd', 'abc'))
 print(set_from_tuples)
 
 numbers = [1,2,3,1,2,3,4]
