c9665a056d139d0d150c3818038e2830256805ac
fmntnz <C0ndeAstar0th@gmail.com>
2023-02-10 16:34:48 -0500

    Class03

--- 03_operations.py
+++ 03_operations.py
@@ -1,18 +1,22 @@
 set_a = {'col', 'mex', 'bol'}
 set_b = {'pe', 'bol'}
 
+#Operator Union
 set_c = set_a.union(set_b)
 print(set_c)
 print(set_a | set_b)
 
+#Operator Intersection
 set_c = set_a.intersection(set_b)
 print(set_c)
 print(set_a & set_b)
 
+#First Set without common elements with second set
 set_c = set_a.difference(set_b)
 print(set_c)
 print(set_a - set_b)
 
+#Union without common elements
 set_c = set_a.symmetric_difference(set_b)
 print(set_c)
 print(set_a ^ set_b)
